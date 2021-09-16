afcb9ac341b82ddef31f2c03aacee8252d69d480
Alex Hall <analytics@alex-hall.co.uk>
2021-09-16 14:51:05 +0100

    first pass at fetching trade data

--- get-binance-trades.py
+++ get-binance-trades.py
@@ -5,10 +5,10 @@ Created on Wed Sep 15 19:05:03 2021
 @author: Alex
 """
 
-#TODO - automatically generate trade pairs and save in matrix
-#sum the trade values to get net trade for each pair
+#TODO - check pairs exist on binance before looping through every combination
 
 import os
+import pandas as pd
 from binance.client import Client
 
 
@@ -16,9 +16,11 @@ api_key = os.environ.get('binance_api')
 api_secret = os.environ.get('binance_secret')
 
 #list of top 20 for now. Use seperate api to automatically populate this later
-CURRENCIES = ['USDT', 'BTC', 'ADA', 'BNB', 'XRP', 'SOL', 'DOT', 'DOGE',
+CURRENCIES = ['ETH', 'BTC', 'USDT', 'ADA', 'BNB', 'XRP', 'SOL', 'DOT', 'DOGE',
               'USDC', 'BUSD', 'LUNA', 'LINK','UNI', 'LTC', 'BCH', 'AVAX',
-              'ALGO', 'ICP', 'WBTC', 'ATOM']
+              'ALGO', 'ICP', 'ATOM']
+
+
 
 LIMIT = 100 #max number of trades per pair
 
@@ -95,13 +97,24 @@ def filter_cutoff_time(trades, cut_time):
 
 
     
-def get_qty_from_trades(trades, limit):
+def get_qty_from_trades(trades):
     """
-    return trade sizes from list of trades and zero pad end if less than limit
+    return trade sizes from list of trades. uses isbuyermaker flag to define
+    sells and buys using
+    
+    isBuyerMaker = true -> SELL
+    isBuyerMaker = false -> BUY
 
     """
     
     qtys = [d['qty'] for d in trades]
+    isBuyerMaker = [d['isBuyerMaker'] for d in trades]
+    isBuyerMaker = [int(i) for i in isBuyerMaker]
+    qtys = [float(i) for i in qtys]
+
+    #multiply sells by -1 and buys by 1
+    isBuyerMaker = [((-2*i)+1) for i in isBuyerMaker]  #convert to + and -1
+    qtys = [a*b for a,b in zip(qtys,isBuyerMaker)]
     
     #probably dont need this if data is to be stored in a matrix
    # if(len(qtys) < limit):
@@ -121,4 +134,26 @@ if __name__ == "__main__":
     
     cutoff_time = get_max_time(top_pair_trades, LIMIT-1)
     
+    
+    results_df = pd.DataFrame(columns = CURRENCIES, index=CURRENCIES)
+    
+    
+    for i in range(len(CURRENCIES)):
+        for j in(range(len(CURRENCIES))):
+            if i != j:
+                
+                curr1 = CURRENCIES[i]
+                curr2 = CURRENCIES[j]
+                pair = curr1 + curr2
+                print(pair)
+                
+                try:
+                    trades = get_recent_trades(pair, LIMIT)
+                    trades = filter_cutoff_time(trades, cutoff_time)
+                    trade_total = sum(get_qty_from_trades(trades))
+                
+                    results_df[curr1][curr2] = trade_total
+                    
+                except:
+                    continue
 
